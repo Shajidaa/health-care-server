081cb287402289c6fec5c7ea274f83334978442d
Shajidaa <shajidaakterlopa101@gmail.com>
2026-09-11 11:52:08 +0600

    feat(appoinetment):update the book appintment data save to the database

--- src/app/middleware/checkAuth.ts
+++ src/app/middleware/checkAuth.ts
@@ -7,15 +7,16 @@ import { catchAsync } from "../utils/catchAsync";
 import { jwtUtils } from "../utils/jwt";
 import { Role } from "../../../generated/prisma/enums";
 
+export interface RequestUser {
+  email: string;
+  name: string;
+  userId: string;
+  role: Role;
+}
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        email: string;
-        name: string;
-        userId: string;
-        role: Role;
-      };
+      user?: RequestUser;
     }
   }
 }
