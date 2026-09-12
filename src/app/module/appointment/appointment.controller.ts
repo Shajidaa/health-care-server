5fc25a32d84d13cda0521eef7913d3e8aec4051f
Shajidaa <shajidaakterlopa101@gmail.com>
2026-09-12 20:08:17 +0600

    feat(appointment):add cancel appointment add

--- src/app/module/appointment/appointment.controller.ts
+++ src/app/module/appointment/appointment.controller.ts
@@ -27,6 +27,17 @@ const payAppointment = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const cancelAppointment = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+
+  const result = await AppointmentServices.cancelAppointment(payload);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Appointment Cancelled Successfully",
+    data: result,
+  });
+});
 const bookAppointmentCallback = catchAsync(
   async (req: Request, res: Response) => {
     console.log(req.query, "req.query");
@@ -42,5 +53,6 @@ const bookAppointmentCallback = catchAsync(
 export const AppointmentController = {
   bookAppointment,
   payAppointment,
+  cancelAppointment,
   bookAppointmentCallback,
 };
