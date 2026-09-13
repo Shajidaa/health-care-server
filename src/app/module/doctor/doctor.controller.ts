7e98896980927e4fef6a52cf1d8ae56cbb2ac28b
Shajidaa <shajidaakterlopa101@gmail.com>
2026-09-13 20:14:20 +0600

    feat(doctor):add upload fields into the apply as doctor route

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -5,12 +5,19 @@ import { doctorService } from "./doctor.service";
 import httpStatus from "http-status";
 
 const applyAsDoctor = catchAsync(async (req: Request, res: Response) => {
-  const result = await doctorService.applyAsDoctor();
+  // const result = await doctorService.applyAsDoctor();
+
+  const data = req.body;
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  console.log({ files });
+  const resume = files?.["resume"] ? files["resume"][0] : null;
+  const additionalFiles = files?.["additionalFiles"] || [];
+  console.log({ files, data, resume, additionalFiles });
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Doctor application submitted successfully",
-    data: result,
+    data: {},
   });
 });
 export const doctorController = {
--- src/app/module/doctor/doctor.route.ts
+++ src/app/module/doctor/doctor.route.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { doctorController } from "./doctor.controller";
+import { upload } from "../../lib/multer";
 
 const router = Router();
 
-router.post("/", doctorController.applyAsDoctor);
+router.post(
+  "/apply-as-doctor",
+  upload.fields([
+    { name: "resume", maxCount: 1 },
+    { name: "additionalFiles", maxCount: 5 }, // Adjust maxCount as needed
+  ]),
+  doctorController.applyAsDoctor,
+);
 
 export const DoctorRoutes = router;
